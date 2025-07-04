d34b5c964e5ac789dcc11ae59105a90aaf155100
Marina <marina.philogene@gmail.com>
2025-07-04 15:36:50 +0200

    test des modifications

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,10 +8,15 @@ function editNav() {
 }
 
 // DOM Elements
+const form = document.querySelector('form[name="reserve"]');
 const modalbg = document.querySelector(".bground");
+const modalbgValidation = document.querySelector(".bgroundValidation");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalValidation = document.querySelector(".modal-validation");
 
+const BtncloseModal = document.querySelector(".btn-submit");
+BtncloseModal.addEventListener("click", validate);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -33,31 +38,97 @@ function closeModal() {
 //validate
 function validate(e) {
   e.preventDefault();
-
-  // Récupération des éléments du formulaire 
+  //informations
   const firstName = document.querySelector("#first");
-  const lastName = document.querySelector("#last");
-  const email = document.querySelector("#email");
-  const birthdate = document.querySelector("#birthdate");
-  const quantity = document.querySelector("#quantity");
-  const selectedLocation = document.querySelector("#location");
-  const checkbox = document.querySelector("#checkbox");
-
-  if (firstName.value.trim() === "") {
-    firstName.parentElement.setAttribute("data-error", "Le prénom est requis");
-    firstName.parentElement.setAttribute("data-error-visible", "true");
+  const lastName = document.querySelector('#last');
+  const email = document.querySelector('#email');
+  const birthdate = document.querySelector('#birthdate');
+  const NbrTournoi = document.querySelector('#quantity');
+  const checkbox1 = document.querySelector("#checkbox1");
+  const radios = document.getElementsByName("locationTournoi");
+  let checked = false;
+  let formValid = true;
+  // Vérification du prénom
+  if (firstName.value.trim().length < 2) {
+    formData[0].setAttribute("data-error-visible", "true");
+    formValid = false;
   } else {
-    firstName.parentElement.removeAttribute("data-error");
-    firstName.parentElement.setAttribute("data-error-visible", "false");
+    formData[0].setAttribute("data-error-visible", "false");
+
+  }
+  // Vérification du nom
+  if (lastName.value.trim().length < 2) {
+    formData[1].setAttribute("data-error-visible", "true");
+    formValid = false;
+  }
+  else {
+    formData[1].setAttribute("data-error-visible", "false");
+  }
+  // Vérification de l'e-mail
+  if (email.value.trim().length < 2) {
+    formData[2].setAttribute("data-error-visible", "true");
+    formValid = false;
+  }
+  else {
+    formData[2].setAttribute("data-error-visible", "false");
+  }
+  // Vérification de la date de naissance 
+  if (birthdate.value.trim() === "") {
+    formData[3].setAttribute("data-error-visible", "true");
+    formValid = false;
+  }
+  else {
+    formData[3].setAttribute("data-error-visible", "false");
+  }
+  // Vérification du nombre de tournoi
+  if (NbrTournoi.value.trim() === "") {
+    formData[4].setAttribute("data-error-visible", "true");
+    formValid = false;
+  }
+  else {
+    formData[4].setAttribute("data-error-visible", "false");
+  }
+  // Vérification du Lieu du prochain tournoi
+  for (let radio of radios) {
+    if (radio.checked) {
+      checked = true;
+      break; // On sort dès qu’on en trouve un sélectionné
+    }
   }
 
-
-  if (lastName.value.trim() === "") {
-    lastName.parentElement.setAttribute("data-error", "Le nom est requis");
-    lastName.parentElement.setAttribute("data-error-visible", "true");
+  if (checked) {
+    formData[5].setAttribute("data-error-visible", "false");
+    formValid = false;
   } else {
-    lastName.parentElement.removeAttribute("data-error");
-    lastName.parentElement.setAttribute("data-error-visible", "false");
+    formData[5].setAttribute("data-error-visible", "true");
+
+  }
+  // Vérification des conditions d'utilisation
+  if (!checkbox1.checked) {
+    formData[6].setAttribute("data-error-visible", "true");
+    formValid = false;
+
+  } else {
+    formData[6].setAttribute("data-error-visible", "false");
+
   }
 
+  // Afficher les informations dans la console
+  console.log(firstName.value,
+    lastName.value,
+    email.value,
+    birthdate.value,
+    NbrTournoi.value,
+    Array.from(radios).find(radio => radio.checked)?.value,
+    checkbox1.checked);
+
+  console.log("Validation du formulaire");
+
+  // Afficher la modal de validation si tout est valide
+  if (!formValid) {
+    e.preventDefault();
+    modalbg.style.display = "none";
+    modalbgValidation.style.display = "block";
+    modalValidation.style.display = "block";
+  }
 }
